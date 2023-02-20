0ec4ed27c7321d13927a754507c89b52214ea3cb
LeivaDiego <lei21752@uvg.edu.gt>
2023-02-20 08:06:31 -0600

    Version 2.0 Interfaces pila y lista
    Diseño de interfaces para las pilas y las listas a implementar

--- InfixCalculator/src/IList.java
+++ InfixCalculator/src/IList.java
@@ -1,2 +1,25 @@
+/**
+ * Interfaz para las listas
+ * @param <T> dato generico
+ * @author diego leiva
+ * Referencia de malonso-uvg
+ */
 public interface IList<T> {
+    void InsertAtStart(T value);
+
+    void InsertAtEnd(T value);
+
+    void Insert(T value, int index);
+
+    T Delete(int index);
+
+    T DeleteAtStart();
+
+    T DeleteAtEnd();
+
+    T Get(int index);
+
+    boolean IsEmpty();
+
+    int Count();
 }
