2e3d936c382213ea4976ad239f288666cb765870
LeivaDiego <lei21752@uvg.edu.gt>
2023-02-20 17:53:51 -0600

    Version 9.0 Main y View
    Primer diseño de las clase main y la clase de vista. Aun sin los procesos a implementar

--- InfixCalculator/src/Main.java
+++ InfixCalculator/src/Main.java
@@ -1,5 +1,56 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        View view = new View();
+        int option = 0;
+        while (option >4 || option <1) {
+            view.ImplementationMenu();
+            try {
+                option = Integer.parseInt(input.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("ERROR: Debe ingresar solo un numero entero");
+            }
+            if (option < 1 || option > 4) {
+                System.out.println("ERROR: Opcion invalida");
+                continue;
+            }
+            switch (option){
+                case 1:
+                    System.out.println("Selecciono la opcion 1");
+                    break;
+                case 2:
+                    System.out.println("Selecciono la opcion 2");
+                    break;
+                case 3:
+                    int type =0;
+                    while (type>2 || type<1){
+                        view.ListMenu();
+                        try {
+                            type = Integer.parseInt(input.nextLine());
+                        }catch (NumberFormatException e){
+                            System.out.println("ERROR: Debe ingresar solo un numero entero");
+                        }
+                        if (type < 1 || type >2){
+                            System.out.println("ERROR: Opcion Invalida");
+                            continue;
+                        }
+                        switch (type){
+                            case 1:
+                                System.out.println("Selecciono opcion 1");
+                                break;
+                            case 2:
+                                System.out.println("Selecciono opcion 2");
+                                break;
+                        }
+                    }
+                    break;
+                case 4:
+                    view.Exit();
+                    break;
+            }
+
+        }
     }
 }
--- InfixCalculator/src/View.java
+++ InfixCalculator/src/View.java
@@ -1,2 +1,23 @@
 public class View {
+
+    public void ImplementationMenu(){
+        System.out.println("Bienvenido a la calculadora de expresiones INFIX");
+        System.out.println("Seleccione una opcion para la implementacion de la pila\n");
+        System.out.println("[1] ArrayList");
+        System.out.println("[2] Vector");
+        System.out.println("[3] Lista");
+        System.out.println("[4] Salir\n");
+
+    }
+    public void ListMenu(){
+        System.out.println("Seleccione una opcion para la implementacion de la lista\n");
+        System.out.println("[1] Lista Simplemente encadenada");
+        System.out.println("[2] Lista Doblemente encadenada\n");
+
+    }
+
+    public void Exit(){
+        System.out.println("...");
+        System.out.println("Ha salido del Programa");
+    }
 }
