0dccc0235fc356c01b54b6e1f6da3304c2e6ad2a
LeivaDiego <lei21752@uvg.edu.gt>
2023-02-20 11:21:48 -0600

    Version 4.0 Interfaz calculadora Postfix
    Diseño de la interfaz de la calculadora de expresiones postfix

--- InfixCalculator/src/IPostfixCalculator.java
+++ InfixCalculator/src/IPostfixCalculator.java
@@ -1,2 +1,15 @@
-public interface IPostfixCalculator {
+/**
+ * @author diego leiva
+ * Interfaz para las operaciones postfix
+ * @param <T> dato generico
+ * Referencia interfaz creada por la seccion 40 en la hoja de trabajo 2
+ */
+public interface IPostfixCalculator<T> {
+    /**
+     * Metodo de operaciones
+     * @param postfix_expression un arraylist generico
+     * @return un valor de tipo int con el resultado de la operacion
+     * @throws Exception
+     */
+    public int Calculate(java.util.ArrayList<T> postfix_expression) throws Exception;
 }
