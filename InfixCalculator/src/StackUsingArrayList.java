dfbb82729c5d144848295ec4e8550c817ea72574
LeivaDiego <lei21752@uvg.edu.gt>
2023-02-20 12:10:27 -0600

    Version 6.0 Pila implementada con ArrayList
    Implementacion de una pila usando un arrayList a partir de su clase abstracta.

--- InfixCalculator/src/StackUsingArrayList.java
+++ InfixCalculator/src/StackUsingArrayList.java
@@ -1,2 +1,35 @@
-public class StackUsingArrayList{
+import java.util.ArrayList;
+
+public class StackUsingArrayList<T> extends Stack<T>{
+
+    private ArrayList<T> arrayList;
+
+    public StackUsingArrayList(){
+        arrayList = new ArrayList<T>();
+    }
+
+    @Override
+    public void push(T value) {
+        arrayList.add(0,value);
+    }
+
+    @Override
+    public T pull() {
+        return arrayList.remove(0);
+    }
+
+    @Override
+    public T peek() {
+        return arrayList.get(0);
+    }
+
+    @Override
+    public int count() {
+        return arrayList.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return arrayList.isEmpty();
+    }
 }
